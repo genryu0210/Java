d35e0aef5b59b43dccda5583d8a3b3706d5f3385
genryu0210 <72203347+genryu0210@users.noreply.github.com>
2022-06-15 17:22:18 +0900

    Update No041.java

--- math_algorythm/No041.java
+++ math_algorythm/No041.java
@@ -1,10 +1,27 @@
 package math_and_algorithm;
 
+import java.util.Scanner;
 public class No041 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt(),N = sc.nextInt();
+		int[] L = new int[N],R = new int[N],Tnum = new int[T+1];
+		
+		for(int i=0;i<N;i++) {
+			L[i] = sc.nextInt();
+			R[i] = sc.nextInt();
+			Tnum[L[i]]++;
+			Tnum[R[i]]--;
+		}
+		Ans(T,Tnum);
 	}
-
+	public static  void Ans(int T,int Tnum[]) {
+		int Ans = 0;
+		for(int i=0;i<T;i++) {
+			Ans += Tnum[i];
+			System.out.println(Ans);
+		}
+	}
 }
